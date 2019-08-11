c158e97c17870904be491e61b30d7dbcc32a698e
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-11 19:56:03 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
-  
+  arrr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all = self
+  end
+
+  def self.all
+    @@all 
+  end
+
 end
