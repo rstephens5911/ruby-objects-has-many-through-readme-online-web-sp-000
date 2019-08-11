d04269796279ea8125427a5d74070527c3370641
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-11 20:06:46 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,7 +13,8 @@ class Waiter
     @@all
   end
 
-  def new_meal(self, customer, total, tip = 0) # self, here, is the waiter
+  def new_meal(customer, total, tip = 0)  
+    Meal.new(self, customer, total, tip = 0)  # self, here, is the waiter
   end
 
 end
